0936cac6c33a2bd44c617fcc45f6040aae29989f
Sean Sutton <sean@thinknimble.com>
2019-07-16 16:37:36 -0400

    switching repo

---
+++ RydeV2/App.js
@@ -0,0 +1,100 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button
+} from 'react-native'
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions
+} from 'react-native/Libraries/NewAppScreen'
+import { throwStatement } from '@babel/types'
+
+export default class App extends Component {
+  state = {
+    placeName: '',
+    places: []
+  }
+
+  placeNameChangeHandler = (val) => {
+    this.setState({
+      placeName: val
+    })
+  }
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === '') {
+      return
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
+  }
+
+  render () {
+    const places = this.state.places.map((place, index) => (
+      <Text key={index}>{place}</Text>
+    ))
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="Ryde V2"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+          />
+        </View>
+
+        <Button
+          onPress={this.placeSubmitHandler}
+          style={styles.placeButton}
+          title="Add"
+        />
+        <View>
+          {places}
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  inputContainer: {
+    width: '100%',
+    paddingTop: 10,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: '70%'
+  },
+  placeButton: {
+    width: '30%'
+  }
+})
